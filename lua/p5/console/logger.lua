ba46d888b65e7eed4d894d833a002ee6e4fccb09
xmlwizard <xml-wizard@outlook.com>
2026-01-21 14:50:53 +0200

    removed everything...start from scratch

--- lua/p5/console/logger.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
--- Placeholder for console logger
-function M.toggle_console()
-  vim.notify("Console logging not yet implemented - Phase 3 feature", vim.log.levels.INFO)
-end
-
-return M
