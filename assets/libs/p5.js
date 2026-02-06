7d877aefc15b8c5d3bd42e18a948260118c5f9e6
iseeheaven <xml-wizard@outlook.com>
2026-02-06 17:35:58 +0000

    Update p5.js types and core modules (2026-02-06 17:35:58)

---
+++ assets/libs/p5.js
@@ -0,0 +1,7 @@
+// p5.js Core Library
+// This is a placeholder - in the actual workflow this will be the unminified p5.js
+// Generated: 2026-02-06T17:35:57.792Z
+// Source: https://github.com/processing/p5.js
+
+// Placeholder content - will be replaced with actual p5.js library
+console.log('p5.js library placeholder');
