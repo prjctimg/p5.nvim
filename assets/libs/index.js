de023dcaa8143fefa610b3cc7c0ac52a18c7632b
iseeheaven <xml-wizard@outlook.com>
2026-02-06 18:39:16 +0000

    Update p5.js types and core modules (2026-02-06 18:39:16)

---
+++ assets/libs/index.js
@@ -0,0 +1,6 @@
+// p5.js Core Modules
+// Generated: 2026-02-06T18:39:16.182Z
+// Source: https://github.com/processing/p5.js
+
+export { default as p5 } from './p5.js';
+export { default as p5Sound } from './p5.sound.js';
---
+++ assets/libs/p5.sound.js
@@ -0,0 +1,7 @@
+// p5.sound.js Library
+// This is a placeholder - in the actual workflow this will be the unminified p5.sound.js
+// Generated: 2026-02-06T18:39:16.182Z
+// Source: https://github.com/processing/p5.js
+
+// Placeholder content - will be replaced with actual p5.sound.js library
+console.log('p5.sound.js library placeholder');
