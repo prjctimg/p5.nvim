6a6d02248007befdafc82ae1e39ad89df01660f0
iseeheaven <xml-wizard@outlook.com>
2026-02-07 07:09:28 +0000

    Update p5.js types and core modules (2026-02-07 07:09:28)

--- assets/libs/p5.js
+++ assets/libs/p5.js
@@ -1,7 +1,61 @@
-// p5.js Core Library
-// This is a placeholder - in the actual workflow this will be the unminified p5.js
-// Generated: 2026-02-07T06:32:23.077Z
-// Source: https://github.com/processing/p5.js
+// core
+import p5 from './core/main';
 
-// Placeholder content - will be replaced with actual p5.js library
-console.log('p5.js library placeholder');
+// shape
+import shape from './shape';
+shape(p5);
+
+//accessibility
+import accessibility from './accessibility';
+accessibility(p5);
+
+// color
+import color from './color';
+color(p5);
+
+// core
+// currently, it only contains the test for parameter validation
+import friendlyErrors from './core/friendly_errors';
+friendlyErrors(p5);
+
+// data
+import data from './data';
+data(p5);
+
+// DOM
+import dom from './dom';
+dom(p5);
+
+// events
+import events from './events';
+events(p5);
+
+// image
+import image from './image';
+image(p5);
+
+// io
+import io from './io';
+io(p5);
+
+// math
+import math from './math';
+math(p5);
+
+// utilities
+import utilities from './utilities';
+utilities(p5);
+
+// webgl
+import webgl from './webgl';
+webgl(p5);
+
+// typography
+import type from './type'
+type(p5);
+
+import { waitForDocumentReady, waitingForTranslator, _globalInit } from './core/init';
+Promise.all([waitForDocumentReady(), waitingForTranslator]).then(_globalInit);
+
+export default p5;
+
