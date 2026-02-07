bc686b5966acf70400dc5e3eddca8ad77a9da7cc
xmlwizard <xml-wizard@outlook.com>
2026-02-07 08:04:31 +0200

    removed assets

--- assets/libs/index.js
+++
@@ -1,6 +0,0 @@
-// p5.js Core Modules
-// Generated: 2026-02-07T02:23:20.092Z
-// Source: https://github.com/processing/p5.js
-
-export { default as p5 } from './p5.js';
-export { default as p5Sound } from './p5.sound.js';
