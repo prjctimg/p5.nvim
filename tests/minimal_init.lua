073e3aef479c1001f83998e5ac30b63b01c350a0
Dean Tarisai <prjctimg@outlook.com>
2026-04-06 02:15:40 +0200

    removed tests directory

--- tests/minimal_init.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.runtimepath:append(".")
-vim.opt.runtimepath:append("..")
-vim.opt.runtimepath:append(vim.fn.expand("~/.local/share/nvim/lazy/plenary.nvim"))
-
-package.path = "./lua/?.lua;./lua/?/init.lua;~/.local/share/nvim/lazy/plenary.nvim/lua/?.lua;~/.local/share/nvim/lazy/plenary.nvim/lua/?/init.lua;" .. package.path
